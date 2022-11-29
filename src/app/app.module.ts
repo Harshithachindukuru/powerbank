1d0b5219a61ec830fc99ddfe69caa794b35dc05d
Harshitha chindukuru <harshithachindukuru@gmail.com>
2022-11-29 18:47:21 +0530

    buy&sell

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { CustomerViewComponent } from './customer-view/customer-view.component';
+import { BuyComponent } from './buy/buy.component';
+import { SellComponent } from './sell/sell.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    CustomerViewComponent
+    CustomerViewComponent,
+    BuyComponent,
+    SellComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
